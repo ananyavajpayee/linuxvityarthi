862fe8799b13c0c3565feb83854cbd702b0b087f
ananya_vajpayee <ananya.24bce10584@vitbhopal.ac.in>
2026-03-30 10:19:31 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 # Script 1: System Identity Report
 # Author: [Your Name] | Course: Open Source Software
 # --- Variables ---
-STUDENT_NAME="Abhijoy Paul" # Fill in your name
+STUDENT_NAME="Ananya" # Fill in your name
 SOFTWARE_CHOICE="Python" # Fill in your chosen software
 # --- System info ---
 KERNEL=$(uname -r)
